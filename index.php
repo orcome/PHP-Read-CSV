ac59de47f41fff6bfb545c9f7a36e3df429f0608
Akhmad Herdian <akhmad.herdian@gmail.com>
2021-09-23 17:29:30 +0800

    Add file csv and show in table

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
 <body>
     <center>
         <h2>Read CSV Data With PHP</h2>
+
+        <?php
+            echo '<table border="1">';
+            $startRow = 1;
+            if (($csvFile = fopen("Test.csv", "r")) !== false) {
+                while (($readData = fgetcsv($csvFile, 1000, ",")) !== false) {
+                    $columnCount = count($readData);
+                    echo '<tr>';
+
+                    $startRow++;
+                    for ($cellData = 0; $cellData < $columnCount; $cellData++) {
+                        echo '<td>'.$readData[$cellData].'</td>';
+                    }
+
+                    echo '</tr>';
+                }
+                fclose($csvFile);
+            }
+            echo '</table>';
+        ?>
+
     </center>
 </body>
 </html>
